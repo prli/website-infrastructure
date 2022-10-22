efa1f927fe140d0cc92da5a110f0f9226ba2d9f2
Richard Li <richprli@amazon.com>
2022-10-22 16:04:31 -0700

    fixed CORS error on mock response endpoint

--- lib/api-gateway-resources.ts
+++ lib/api-gateway-resources.ts
@@ -15,15 +15,24 @@ export class ApiGatewayResources extends Construct {
 
     const resource = props.apiGateway.root.addResource(props.resourcePath);
 
-    resource.addMethod('GET', new apigateway.MockIntegration({
+    resource.addMethod('POST', new apigateway.MockIntegration({
       integrationResponses: [
         {
           statusCode: "200",
           responseTemplates: {
-            "application/json": JSON.stringify({
-              "id": "$context.requestId",
-              "createdAt": "$context.requestTime",
-            }, null, 4)
+            "application/json": JSON.stringify([
+              {
+                "id": "$context.requestId",
+                "createdAt": "$context.requestTime",
+              },
+            ], null, 4)
+          },
+          // Required for CORS
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            'method.response.header.Access-Control-Allow-Origin': "'*'",
+            'method.response.header.Access-Control-Allow-Credentials': "'false'",
+            'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
           },
         }
       ],
@@ -33,7 +42,55 @@ export class ApiGatewayResources extends Construct {
       },
     }), {
       methodResponses: [
-        { statusCode: '200' },
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': false,
+            'method.response.header.Access-Control-Allow-Methods': false,
+            'method.response.header.Access-Control-Allow-Credentials': false,
+            'method.response.header.Access-Control-Allow-Origin': false,
+          },
+        },
+      ],
+      authorizer: props.authorizer,
+    });
+
+    resource.addMethod('GET', new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseTemplates: {
+            "application/json": JSON.stringify([
+              {
+                "id": "$context.requestId",
+                "createdAt": "$context.requestTime",
+              },
+            ], null, 4)
+          },
+          // required for CORS
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            'method.response.header.Access-Control-Allow-Origin': "'*'",
+            'method.response.header.Access-Control-Allow-Credentials': "'false'",
+            'method.response.header.Access-Control-Allow-Methods': "'OPTIONS,GET,PUT,POST,DELETE'",
+          },
+        }
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': '{ "statusCode": 200 }',
+      },
+    }), {
+      methodResponses: [
+        {
+          statusCode: '200',
+          responseParameters: {
+            'method.response.header.Access-Control-Allow-Headers': false,
+            'method.response.header.Access-Control-Allow-Methods': false,
+            'method.response.header.Access-Control-Allow-Credentials': false,
+            'method.response.header.Access-Control-Allow-Origin': false,
+          },
+        },
       ],
       authorizer: props.authorizer,
     });
